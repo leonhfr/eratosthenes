edf3835f5205027293a461c1ced9c9093c1d2b3c
leonhfr <hello@leonh.fr>
2019-03-15 13:56:01 +0100

    [IMPROVEMENT] Static client and fixes

--- src/drivers/lambda.ts
+++ src/drivers/lambda.ts
@@ -5,16 +5,16 @@ import * as AWS from 'aws-sdk';
 // Code.
 const debugVerbose = debug('eratosthenes:verbose:drivers:lambda');
 
-const client = new AWS.Lambda();
-
 export class Lambda {
+  static client = new AWS.Lambda();
+
   // ignoring wrapping function
   /*istanbul ignore next*/
   static async invoke(
     request: AWS.Lambda.InvocationRequest
   ): Promise<AWS.Lambda.InvocationResponse> {
     debugVerbose(`invoke input: %j`, request);
-    const response = await client.invoke(request).promise();
+    const response = await this.client.invoke(request).promise();
     debugVerbose(`invoke response: %j`, response);
     return response;
   }
--- src/drivers/s3.ts
+++ src/drivers/s3.ts
@@ -5,17 +5,28 @@ import * as AWS from 'aws-sdk';
 // Code.
 const debugVerbose = debug('eratosthenes:verbose:drivers:s3');
 
-const client = new AWS.S3();
-
 export class S3 {
+  static client = new AWS.S3();
+
   // ignoring wrapping function
   /*istanbul ignore next*/
   static async getObject(
     request: AWS.S3.GetObjectRequest
   ): Promise<AWS.S3.GetObjectOutput> {
     debugVerbose(`getObject input: %j`, request);
-    const response = await client.getObject(request).promise();
+    const response = await this.client.getObject(request).promise();
     debugVerbose(`getObject response: %j`, response);
+    return response;
+  }
+
+  // ignoring wrapping function
+  /*istanbul ignore next*/
+  static async putObject(
+    request: AWS.S3.PutObjectRequest
+  ): Promise<AWS.S3.PutObjectOutput> {
+    debugVerbose(`putObject input: %j`, request);
+    const response = await this.client.putObject(request).promise();
+    debugVerbose(`putObject response: %j`, response);
     return response;
   }
 }
