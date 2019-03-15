57bbdf306fb5fd391e58cc583e701d07d2a2855d
léon h <hello@leonh.fr>
2019-03-15 13:57:08 +0100

    [IMPROVEMENT] Static client and fixes (#6)

--- src/drivers/dynamodb.ts
+++ src/drivers/dynamodb.ts
@@ -5,16 +5,16 @@ import * as AWS from 'aws-sdk';
 // Code.
 const debugVerbose = debug('eratosthenes:verbose:drivers:dynamodb');
 
-const client = new AWS.DynamoDB.DocumentClient();
-
 export class DynamoDB {
+  static client = new AWS.DynamoDB.DocumentClient();
+
   // ignoring wrapping function
   /*istanbul ignore next*/
   static async get<T>(
     input: AWS.DynamoDB.DocumentClient.GetItemInput
   ): Promise<T> {
     debugVerbose(`get input: %j`, input);
-    const response = await client.get(input).promise();
+    const response = await this.client.get(input).promise();
     debugVerbose(`get response: %j`, response);
     return response.Item as T;
   }
@@ -25,7 +25,7 @@ export class DynamoDB {
     input: AWS.DynamoDB.DocumentClient.PutItemInput
   ): Promise<T> {
     debugVerbose(`put input: %j`, input);
-    const response = await client.put(input).promise();
+    const response = await this.client.put(input).promise();
     debugVerbose(`put response: %j`, response);
     return input.Item as T;
   }
@@ -36,7 +36,7 @@ export class DynamoDB {
     input: AWS.DynamoDB.DocumentClient.UpdateItemInput
   ): Promise<T> {
     debugVerbose(`update input: %j`, input);
-    const response = await client.update(input).promise();
+    const response = await this.client.update(input).promise();
     debugVerbose(`update response: %j`, response);
     return response.Attributes as T;
   }
@@ -47,7 +47,7 @@ export class DynamoDB {
     input: AWS.DynamoDB.DocumentClient.DeleteItemInput
   ): Promise<T> {
     debugVerbose(`delete input: %j`, input);
-    const response = await client.delete(input).promise();
+    const response = await this.client.delete(input).promise();
     debugVerbose(`delete response: %j`, response);
     return input.Key as T;
   }
@@ -56,25 +56,25 @@ export class DynamoDB {
   /*istanbul ignore next*/
   static async scan<T>(
     input: AWS.DynamoDB.DocumentClient.ScanInput
-  ): Promise<T> {
+  ): Promise<Array<T>> {
     debugVerbose(`scan input: %j`, input);
-    const response: AWS.DynamoDB.DocumentClient.ScanOutput = await client
+    const response: AWS.DynamoDB.DocumentClient.ScanOutput = await this.client
       .scan(input)
       .promise();
     debugVerbose(`scan response: %j`, response);
-    return response as T;
+    return response.Items as Array<T>;
   }
 
   // ignoring wrapping function
   /*istanbul ignore next*/
   static async query<T>(
     input: AWS.DynamoDB.DocumentClient.QueryInput
-  ): Promise<T> {
+  ): Promise<Array<T>> {
     debugVerbose(`query input: %j`, input);
-    const response: AWS.DynamoDB.DocumentClient.QueryOutput = await client
+    const response: AWS.DynamoDB.DocumentClient.QueryOutput = await this.client
       .query(input)
       .promise();
     debugVerbose(`query response: %j`, response);
-    return response as T;
+    return response.Items as Array<T>;
   }
 }
--- src/drivers/sqs.ts
+++ src/drivers/sqs.ts
@@ -5,16 +5,16 @@ import * as AWS from 'aws-sdk';
 // Code.
 const debugVerbose = debug('eratosthenes:verbose:drivers:sqs');
 
-const client = new AWS.SQS();
-
 export class SQS {
+  static client = new AWS.SQS();
+
   // ignoring wrapping function
   /*istanbul ignore next*/
   static async sendMessage(
     request: AWS.SQS.SendMessageRequest
   ): Promise<AWS.SQS.SendMessageResult> {
     debugVerbose(`sendMessage input: %j`, request);
-    const response = await client.sendMessage(request).promise();
+    const response = await this.client.sendMessage(request).promise();
     debugVerbose(`sendMessage response: %j`, response);
     return response;
   }
@@ -25,7 +25,7 @@ export class SQS {
     request: AWS.SQS.SendMessageBatchRequest
   ): Promise<AWS.SQS.SendMessageBatchResult> {
     debugVerbose(`sendMessageBatch input: %j`, request);
-    const response = await client.sendMessageBatch(request).promise();
+    const response = await this.client.sendMessageBatch(request).promise();
     debugVerbose(`sendMessageBatch response: %j`, response);
     return response;
   }
