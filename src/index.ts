fc5b8eb0a98d5bf6e2d41094258bd12b5b85276c
leonhfr <hello@leonh.fr>
2019-03-22 15:34:49 +0100

    zone files

--- src/index.ts
+++ src/index.ts
@@ -2,4 +2,5 @@ import * as Models from './models';
 
 export namespace Erathostenes {
   export const AreaModel = Models.AreaModel;
+  export const ZoneModel = Models.ZoneModel;
 }
---
+++ src/models/Zone.ts
@@ -0,0 +1,20 @@
+// Packages.
+// import * as AWS from 'aws-sdk';
+// import * as debug from 'debug';
+import { Zone } from '@scenicroutes/wittgenstein';
+// import * as turfHelpers from '@turf/helpers';
+// import * as turfInvariant from '@turf/invariant';
+
+// Internal.
+// import { DynamoDB } from '../clients';
+// import * as Types from '../types';
+
+// Code.
+// const debugError = debug('eratosthenes:error:zone');
+// const debugVerbose = debug('eratosthenes:verbose:zone');
+
+export class ZoneModel {
+  static async list() {}
+
+  static async insert(zone: Zone) {}
+}
