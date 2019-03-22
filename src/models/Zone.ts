ee11f6a6fd1df021e3b32ef888626b24cc8889f5
leonhfr <hello@leonh.fr>
2019-03-22 16:16:44 +0100

    zone model

--- src/models/Zone.ts
+++ src/models/Zone.ts
@@ -5,28 +5,69 @@ import { Zone } from '@scenicroutes/wittgenstein';
 
 // Internal.
 import { DynamoDB } from '../clients';
-// import * as Types from '../types';
+import * as Types from '../types';
 
 // Code.
 const debugError = debug('eratosthenes:error:zone');
 const debugVerbose = debug('eratosthenes:verbose:zone');
 
 export class ZoneModel {
-  static async list() {}
+  static async query(area: string): Promise<Types.Result<Zone, Error> | Error> {
+    const request: AWS.DynamoDB.DocumentClient.QueryInput = {
+      TableName: 'area',
+      IndexName: area,
+    };
+    debugVerbose(`query: requesting %o`, request);
+
+    const ok: Array<Zone> = [];
+    const err: Array<Error> = [];
+
+    try {
+      const response: AWS.DynamoDB.DocumentClient.QueryOutput = await DynamoDB.query(
+        request
+      ).promise();
+
+      debugVerbose(`query: response %o`, response);
+
+      if (!response.Items) {
+        return { ok, err };
+      }
+
+      for (const item of response.Items) {
+        const maybeZone = Zone.create(item);
+
+        maybeZone instanceof Error ? err.push(maybeZone) : ok.push(maybeZone);
+      }
+
+      return { ok, err };
+    } catch (error) {
+      debugError(`query failed: %s`, error.message);
+      return error;
+    }
+  }
 
   static async put(
     zone: Zone
   ): Promise<AWS.DynamoDB.AttributeMap | undefined | Error> {
+    const bboxMap = JSON.parse(JSON.stringify(zone.bbox));
+    const zoneMap = zone.zone
+      ? JSON.parse(JSON.stringify(zone.zone))
+      : undefined;
+
     const request: AWS.DynamoDB.DocumentClient.PutItemInput = {
       TableName: 'area',
       Item: {
         id: { S: zone.id },
         area: { S: zone.area },
-        bbox: { S: JSON.stringify(zone.bbox) },
-        zone: { S: zone.zone ? JSON.stringify(zone.zone) : '' },
+        bbox: { M: bboxMap },
       },
       ReturnValues: 'ALL_OLD',
     };
+
+    if (zoneMap) {
+      request.Item.zone = { M: zoneMap };
+    }
+
     debugVerbose(`put: request %o`, request);
 
     try {
