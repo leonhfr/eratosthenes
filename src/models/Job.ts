dc963f720ea218c1f3a5b70eb944d7ecaecd14f8
leonhfr <hello@leonh.fr>
2019-03-24 17:22:50 +0100

    sqs job

--- src/models/Job.ts
+++ src/models/Job.ts
@@ -1,10 +1,11 @@
 // Packages.
+import * as AWSLambda from 'aws-lambda';
 import * as AWS from 'aws-sdk';
 import * as debug from 'debug';
 import { Job } from '@scenicroutes/wittgenstein';
 
 // Internal.
-import { DynamoDB } from '../clients';
+import { DynamoDB, Sqs } from '../clients';
 import * as Types from '../types';
 
 // Code.
@@ -101,11 +102,65 @@ export class JobModel {
     }
   }
 
-  static async publish() {
-    // publish job to sqs
+  static async publish(
+    url: string,
+    jobs: Array<Job>
+  ): Promise<
+    | Types.Result<
+        AWS.SQS.SendMessageBatchResultEntry,
+        AWS.SQS.BatchResultErrorEntry
+      >
+    | Error
+  > {
+    const request: AWS.SQS.SendMessageBatchRequest = {
+      QueueUrl: url,
+      Entries: jobs.map(job => ({
+        Id: job.id,
+        MessageBody: JSON.stringify(job),
+      })),
+    };
+    debugVerbose(`publish: request %o`, request);
+
+    try {
+      const response: AWS.SQS.SendMessageBatchResult = await Sqs.sendMessageBatch(
+        request
+      ).promise();
+
+      debugVerbose(`publish: response %o`, response);
+
+      return { ok: response.Successful, err: response.Failed };
+    } catch (err) {
+      debugError(`publish failed: %s`, err.message);
+      return err;
+    }
   }
 
-  static async consume() {
-    // consume job from sqs
+  static consume(event: AWSLambda.SQSEvent): Types.Result<Job, Error> {
+    const ok: Array<Job> = [];
+    const err: Array<Error> = [];
+
+    for (const record of event.Records) {
+      const maybeJob = this.parseRecord(record);
+      if (maybeJob instanceof Job) {
+        ok.push(maybeJob);
+      } else {
+        err.push(maybeJob);
+      }
+    }
+
+    return { ok, err };
+  }
+
+  private static parseRecord(record: AWSLambda.SQSRecord): Job | Error {
+    try {
+      const input = JSON.parse(record.body);
+      const { id, page, zone } = input;
+      if (id && page && zone) {
+        return Job.create({ id, page, zone });
+      }
+      return new Error(`Cannot parse job: ${record.body}`);
+    } catch (err) {
+      return err;
+    }
   }
 }
