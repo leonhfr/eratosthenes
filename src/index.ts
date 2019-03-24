c31fd23400902455087ee1e9bfe9cd67bdb09f07
léon h <hello@leonh.fr>
2019-03-24 20:54:54 +0100

    [BUG] Export (#27)

--- src/index.ts
+++ src/index.ts
@@ -3,5 +3,6 @@ import * as Models from './models';
 export namespace Eratosthenes {
   export const AreaModel = Models.AreaModel;
   export const JobModel = Models.JobModel;
+  export const PhotoModel = Models.PhotoModel;
   export const ZoneModel = Models.ZoneModel;
 }
